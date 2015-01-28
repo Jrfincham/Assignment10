8ab3be3d130e334a84070be6706a29e2b6540e82
Jason Robert Fincham <JRFincham@yahoo.com>
2015-01-27 23:03:55 -0500

    Footer complete

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,12 +3,22 @@ repos.forEach( function (item){
   var name = item.name;
   var description = item.description;
   var updated_at = item.updated_at;
+  var language = item.language;
+  var stargazers = item.stargazers_count;
+  var forkscount = item.forks_count;
 
   var listDiv = $('<div></div>');
   $(listDiv).addClass('icons');
-  $(listDiv).append($('<span></span>').text(name).prop('id', 'nameSpan'));
+  $(listDiv).append($('<a href="#"><span></span></a>').text(name).prop('id', 'nameSpan'));
   $(listDiv).append($('<span></span>').text(description).prop('id', 'descriptionSpan'));
   $(listDiv).append($('<span></span>').text(updated_at).prop('id', 'updated_atSpan'));
+  $(listDiv).append($('<span class="octicon octicon-git-branch"></span>').text(forkscount).prop('id', 'forkscount'));
+  $(listDiv).append($('<span class="octicon octicon-star"></span>').text(stargazers).prop('id', 'stargazers'));
+  $(listDiv).append($('<span></span>').text(language).prop('id', 'language'));
+
+
+
+
 
   $('.reposList').append(listDiv);
 });
